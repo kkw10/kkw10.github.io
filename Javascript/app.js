e0336ccb7edfd6443fb786d8b7ca6e02d022a002
kkw_10 <kkw_10@naver.com>
2019-08-11 21:04:47 +0900

    asdf

--- Javascript/app.js
+++
@@ -1,8 +0,0 @@
-import CarouselApp from "./carouselApp.js";
-import SearchApp from "./searchApp.js";
-
-const carouselApp = new CarouselApp();
-const searchApp = new SearchApp();
-
-carouselApp.run();
-searchApp.run();
